75144989fbbba156c1701d4dae485013a07fe542
Charles Truong <charles.truong@cmla.ens-cachan.fr>
2016-01-11 11:55:00 +0100

    Changements mineurs dans PELT Il vaut mieux mémoize dans PELT plutôt que dans la définition de la fonction de coût. Ainsi les fonctions de coût seront compatibles avec dynamic_prog

---
+++ ruptures/pelt/Pelt.py
@@ -0,0 +1,69 @@
+from ruptures.pelt.costs import NotEnoughPoints
+from ruptures.utils.memoizedict import MemoizeDict
+
+
+class Pelt(object):
+    """Contient l'algorithme de parcours des partitions."""
+
+    def __init__(self, error_func, penalty, n, K=0, min_size=2, jump=1):
+        self.error_func = MemoizeDict(error_func)
+        assert isinstance(n, int)
+        assert n > 2  # at least three points
+        self.n = n
+        assert penalty >= 0
+        self.penalty = penalty
+        self.K = K
+        assert min_size > 0
+        self.min_size = min_size
+        self.jump = jump
+        self.R = {min_size: [0]}  # will contain potential changepoints
+        self.cp = {0: list()}  # will contain the changepoint indexes.
+        self.F = {0: - self.penalty}
+        self.chg = list()
+
+    def fit(self):
+        # the actual changepoint detection algorithm.
+
+        start = self.min_size
+        end = self.n
+
+        # we reset some attributes
+        self.R = {start: [0]}  # will contain potential changepoints
+        self.cp = {0: list()}  # will contain the changepoint indexes.
+        self.F = {0: - self.penalty}
+
+        for fin in range(start, end + 1):
+
+            # epoch 1
+            tmp = list()
+            for dernier_debut in self.R[fin]:
+                try:
+                    c = self.error_func(dernier_debut, fin)
+                    tmp.append(
+                        (dernier_debut,
+                         self.F[dernier_debut] + c + self.penalty))
+                except NotEnoughPoints:
+                    print("pas assez de points")
+            t_1, f = min(tmp, key=lambda x: x[1])
+
+            # epoch 2
+            assert fin not in self.F
+            self.F[fin] = f
+
+            # epoch 3
+            self.cp[fin] = self.cp[t_1] + [t_1]
+
+            # epoch 4
+            R_tmp = list()
+            for dernier_debut in self.R[fin] + [fin - self.min_size]:
+                if fin - dernier_debut >= self.min_size:
+                    if dernier_debut in self.F:
+                        if self.F[dernier_debut] + self.error_func(
+                                dernier_debut, fin) + self.K <= self.F[fin]:
+                            R_tmp.append(dernier_debut)
+
+            self.R[fin + 1] = R_tmp
+        self.chg = [temps for temps in self.cp[
+            self.n - 1] if 0 <= temps < self.n]
+
+        return self.cp[end]
---
+++ ruptures/pelt/costs.py
@@ -0,0 +1,76 @@
+import numpy as np
+from ruptures.utils.memoizedict import MemoizeDict
+from numpy.linalg import lstsq
+
+
+class NotEnoughPoints(Exception):
+    """Raise this exception when there is not enough point to calculate a
+    cost function """
+    pass
+
+
+def gaussmean(signal):
+    """
+    Returns the cost function used in Pelt.
+    Here: - max log likelihood of a univariate gaussian random variable.
+    Associated K (see Pelt.__init__): 0
+
+    signal: array of shape (n_points,) or (n_points, 1)
+    """
+    s = signal
+    if s.ndim == 1:
+        s = s.reshape(-1, 1)
+
+    def error_func(start, end):
+        assert 0 <= start <= end
+
+        if end - start < 2:  # we need at least 2 points
+            raise NotEnoughPoints
+
+        sig = s[start:end]
+        m, v = sig.mean(), sig.var()
+        if v == 0:
+            return np.inf
+        res = np.sum((sig - m) ** 2)
+        res /= 2 * v
+        res += (end - start + 1) / 2 * np.log(v)
+        res += (end - start + 1) / 2 * np.log(2 * np.pi)
+        return res
+
+    return error_func
+
+
+def linear_mse(signal):
+    """
+    Returns the cost function used in Pelt.
+    Here: mean squared error when approximating with a linear function.
+    Associated K (see Pelt.__init__): 0
+
+    signal: array of shape (n_points,) or (n_points, 1) or (n_points, d)
+    """
+    s = signal
+    if s.ndim == 1:
+        s = s.reshape(-1, 1)
+
+    def error_func(start, end):
+        assert 0 <= start <= end
+
+        if end - start < 2:  # we need at least 2 points
+            raise NotEnoughPoints
+
+        sig = s[start:end]
+
+        # we regress over the time variable
+        tt = np.arange(start, end)
+        a = np.column_stack((tt, np.ones(tt.shape)))
+        assert a.shape[0] == sig.shape[0]
+        # doing the regression
+        res_lstsq = lstsq(a, sig)
+        assert res_lstsq[1].shape[0] == sig.shape[1]
+
+        # mean squared error
+        res = np.sum(res_lstsq[1]) / sig.shape[0]
+
+        return res
+
+    return error_func
