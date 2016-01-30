9ecb2e6ffa9a27a5ecf162e883645b68bdefb3e5
Charles Truong <charles.truong@cmla.ens-cachan.fr>
2016-01-30 12:33:23 +0100

    Nouveau PELT

--- ruptures/search_methods/pelt.py
+++ ruptures/search_methods/pelt.py
@@ -1,6 +1,8 @@
+import numpy as np
 from ruptures.base import BaseClass
 import abc
 from ruptures.search_methods import MemoizeDict
+from ruptures.costs import NotEnoughPoints
 
 
 class Pelt(BaseClass, metaclass=abc.ABCMeta):
@@ -24,6 +26,7 @@ class Pelt(BaseClass, metaclass=abc.ABCMeta):
         """
         assert penalty >= 0
         assert min_size > 0
+        assert penalty > self.K
         self.penalty = penalty
         self.jump = jump
         self.min_size = min_size
@@ -32,54 +35,69 @@ class Pelt(BaseClass, metaclass=abc.ABCMeta):
         self.search_method = MemoizeDict(self.search_method.func)
 
     def search_method(self):
+        """Search the partition space for segmentation which minimize the
+        penalized cost.
 
-        first_end = self.min_size
-        last_end = self.n
+        Returns:
+            dict: {(start, end): segment cost + penalty}
+        """
+        bkps = np.arange(0, self.n, self.jump)
+        bkps[-1] = self.n
 
-        # we reset some attributes
-        self.R = {first_end: [0]}  # will contain potential changepoints
-        self.cp = {0: list()}  # will contain the changepoint indexes.
-        self.F = {0: - self.penalty}
+        # initialisation
+        self.F = dict()
+        self.F[0] = {}
 
-        for end in range(first_end, last_end + 1):
+        self.R = dict()
+        self.R[0] = [0]
 
-            # epoch 1
-            # costs for the different partitions of [0:end]
-            segment_costs = list()
-            for last_start in self.R[end]:
-                c = self.error(last_start, end)
-                segment_costs.append(
-                    (last_start,
-                     self.F[last_start] + c + self.penalty))
+        for bkp, previous_bkp in zip(bkps[1:], bkps[:-1]):
 
-            t_1, f = min(segment_costs, key=lambda x: x[1])
+            # we get the potential last breakpoints of the segment [0:end]
+            R_tmp = [t for t in self.R[previous_bkp]]
+            R_tmp.extend(t for t in bkps if self.min_size <=
+                         bkp - t < self.min_size + self.jump)
 
-            # epoch 2
-            assert end not in self.F
-            self.F[end] = f
+            # calcul des quantités F(t) + error(t:bkp)
+            error_list = list()
+            costs_list = list()
+            for t in R_tmp:
 
-            # epoch 3
-            self.cp[end] = self.cp[t_1] + [t_1]
+                try:
+                    tmp_error = self.error(t, bkp)
+                    tmp_cost = tmp_error + sum(self.F[t].values())
+                except NotEnoughPoints:
+                    tmp_error = np.inf
+                    tmp_cost = np.inf
 
-            # epoch 4
-            R_tmp = list()
-            for last_start in self.R[end] + [end - self.min_size]:
-                if end - last_start >= self.min_size:
-                    if last_start in self.F:
-                        if self.F[last_start] + self.error(
-                                last_start, end) + self.K <= self.F[end]:
-                            R_tmp.append(last_start)
+                error_list.append(tmp_error)
+                costs_list.append(tmp_cost)
 
-            self.R[end + 1] = R_tmp
-        self.chg = [temps for temps in self.cp[
-            self.n - 1] if 0 <= temps < self.n]
+            min_cost_index, min_cost, min_error = min(
+                zip(R_tmp, costs_list, error_list), key=lambda z: z[1])
 
-        return self.cp[last_end]
+            # on met self.F à jour
+            self.F[bkp] = {**self.F[min_cost_index],
+                           **{(min_cost_index, bkp): min_error + self.penalty}}
+
+            # on met self.R à jour
+            F_bkp = sum(self.F[bkp].values())
+            self.R[bkp] = [
+                t for t,
+                v in zip(
+                    R_tmp,
+                    costs_list) if v +
+                self.K < F_bkp]
+
+            if len(self.R[bkp]) == 0:
+                self.R[bkp] = [0]
+
+        return self.F[self.n]
 
     def fit(self, signal, penalty, jump, min_size):
         self.reset_params(penalty, jump, min_size)
         self.signal = signal
         self.partition = self.search_method()
-        self.bkps = sorted(t for t in self.partition)[1:] + [self.n]
+        self.bkps = sorted(e for (s, e) in self.partition)
 
         return self.bkps
