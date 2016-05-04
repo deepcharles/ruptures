61162eaefc98145de30dbf0cf60436214226000a
Charles <charles.truong@cmla.ens-cachan.fr>
2016-05-04 13:45:30 +0200

    tests: nouveaux tests

--- tests/test_costs.py
+++ tests/test_costs.py
@@ -1,10 +1,10 @@
 import pytest
 import numpy as np
-from itertools import product
+from itertools import product, accumulate
 from ruptures.costs import ConstantMSE, GaussMLE, LinearMLE, HarmonicMSE
 from ruptures.datasets import pw_constant
 from ruptures.costs import NotEnoughPoints
-PENALTIES = [*np.linspace(0.1, 100, 10), np.linspace(0.1, 100, 10)]
+PENALTIES = np.linspace(0.1, 100, 10)
 ALGOS = [ConstantMSE, GaussMLE, LinearMLE, HarmonicMSE]
 
 
@@ -22,14 +22,27 @@ def signal_bkps():
     return signal, bkps
 
 
+def has_holes(partition):
+    """Returns the start and end of a partition. AssertionError if there is a
+    hole."""
+    def fusion(interval1, interval2):
+        assert interval1[1] == interval2[0], "Hole in the partition."
+        return (interval1[0], interval2[1])
+    sorted_intervals = sorted((s, e) for s, e in partition)
+    return list(accumulate(sorted_intervals, fusion))[-1]
+
+
 @pytest.mark.parametrize("penalty, algo", product(PENALTIES, ALGOS))
 def test_pelt(signal_bkps, penalty, algo):
     signal, bkps = signal_bkps
     method = "pelt"
     a = algo(method)
-    a.fit(signal, penalty=penalty, jump=5, min_size=20)
-    assert a.bkps[-1] == signal.shape[
-        0], "The last breakpoint must be equal to the signal length"
+    my_bkps = a.fit(penalty=penalty, signal=signal, jump=5, min_size=20)
+    assert my_bkps[-1] == signal.shape[
+        0], "The last breakpoint must be equal to the signal length."
+
+    assert has_holes(a._partition) == (
+        0, signal.shape[0]), "The partition does not cover the signal."
 
 
 @pytest.mark.parametrize("algo", ALGOS)
@@ -38,14 +51,19 @@ def test_dynp(signal_bkps, algo):
     n_regimes = len(bkps)
     method = "dynp"
     a = algo(method)
-    a.fit(signal, n_regimes=n_regimes, jump=5, min_size=20)
-    assert a.bkps[-1] == signal.shape[
+    my_bkps = a.fit(n_regimes=n_regimes, signal=signal, jump=5, min_size=20)
+    assert my_bkps[-1] == signal.shape[
         0], "The last breakpoint must be equal to the signal length"
 
-    n_regimes = range(2, n_regimes + 1)
-    a.fit(signal, n_regimes=n_regimes, jump=5, min_size=20)
-    assert a.bkps[-1] == signal.shape[
-        0], "The last breakpoint must be equal to the signal length"
+    assert has_holes(a._partition) == (
+        0, signal.shape[0]), "The partition does not cover the signal."
+
+    for n_reg in range(2, n_regimes + 1):
+        my_bkps = a.fit(n_regimes=n_reg, signal=signal, jump=5, min_size=20)
+        assert my_bkps[-1] == signal.shape[
+            0], "The last breakpoint must be equal to the signal length"
+        assert has_holes(a._partition) == (
+            0, signal.shape[0]), "The partition does not cover the signal."
 
 
 @pytest.mark.parametrize("algo", ALGOS)
@@ -56,9 +74,44 @@ def test_exceptions(signal_bkps, algo):
     method = "dynp"
     a = algo(method)
     with pytest.raises(NotEnoughPoints):
-        a.fit(signal, n_regimes=n_regimes, min_size=1)
+        a.fit(n_regimes=n_regimes, signal=signal, min_size=1)
 
     method = "pelt"
     a = algo(method)
     with pytest.raises(NotEnoughPoints):
-        a.fit(signal, penalty=1, min_size=1)
+        a.fit(penalty=1, signal=signal, min_size=1)
+
+
+@pytest.mark.parametrize("algo", ALGOS)
+def test_dynp_bis(signal_bkps, algo):
+    signal, bkps = signal_bkps
+    n_regimes = len(bkps)
+    method = "dynp"
+    a = algo(method)
+    my_bkps = a.fit(n_regimes=n_regimes, signal=signal, jump=5, min_size=20)
+    assert my_bkps[-1] == signal.shape[
+        0], "The last breakpoint must be equal to the signal length"
+
+    assert has_holes(a._partition) == (
+        0, signal.shape[0]), "The partition does not cover the signal."
+
+    for n_reg in range(2, n_regimes + 1):
+        my_bkps = a.fit(n_regimes=n_reg, jump=5, min_size=20)
+        assert my_bkps[-1] == signal.shape[
+            0], "The last breakpoint must be equal to the signal length"
+        assert has_holes(a._partition) == (
+            0, signal.shape[0]), "The partition does not cover the signal."
+
+
+@pytest.mark.parametrize("algo", ALGOS)
+def test_pelt_bis(signal_bkps, algo):
+    signal, bkps = signal_bkps
+    method = "pelt"
+    a = algo(method)
+    my_bkps = a.fit(penalty=1, signal=signal, jump=5, min_size=20)
+    for pen in PENALTIES:
+        my_bkps = a.fit(penalty=pen, jump=5, min_size=20)
+        assert my_bkps[-1] == signal.shape[
+            0], "The last breakpoint must be equal to the signal length."
+        assert has_holes(a._partition) == (
+            0, signal.shape[0]), "The partition does not cover the signal."
--- tests/test_kernel.py
+++ tests/test_kernel.py
@@ -26,8 +26,8 @@ def test_pelt(signal_bkps, penalty, kernel):
     signal, bkps = signal_bkps
     method = "pelt"
     a = KernelMSE(method, kernel)
-    a.fit(signal, penalty=penalty, jump=5, min_size=3)
-    assert a.bkps[-1] == signal.shape[
+    my_bkps = a.fit(penalty=penalty, signal=signal, jump=5, min_size=3)
+    assert my_bkps[-1] == signal.shape[
         0], "The last breakpoint must be equal to the signal length"
 
 
@@ -37,8 +37,8 @@ def test_dynp(signal_bkps, kernel):
     n_regimes = len(bkps)
     method = "dynp"
     a = KernelMSE(method, kernel)
-    a.fit(signal, n_regimes=n_regimes, jump=5, min_size=2)
-    assert a.bkps[-1] == signal.shape[
+    my_bkps = a.fit(n_regimes=n_regimes, signal=signal, jump=5, min_size=2)
+    assert my_bkps[-1] == signal.shape[
         0], "The last breakpoint must be equal to the signal length"
 
 
@@ -50,9 +50,9 @@ def test_exceptions(signal_bkps, kernel):
     method = "dynp"
     a = KernelMSE(method, kernel)
     with pytest.raises(NotEnoughPoints):
-        a.fit(signal, n_regimes=n_regimes, min_size=1)
+        a.fit(signal=signal, n_regimes=n_regimes, min_size=1)
 
     method = "pelt"
     a = KernelMSE(method, kernel)
     with pytest.raises(NotEnoughPoints):
-        a.fit(signal, penalty=1, min_size=1)
+        a.fit(signal=signal, penalty=1, min_size=1)
