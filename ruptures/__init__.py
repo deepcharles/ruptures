cd0d86f5f13e778f15fac7d8a760d443aebfd375
Charles <charles.truong@cmla.ens-cachan.fr>
2016-09-29 17:36:57 +0200

    ajout dans le __init__.py

--- ruptures/__init__.py
+++ ruptures/__init__.py
@@ -1,4 +1,4 @@
 from .costs import NotEnoughPoints
 from .search_methods import METHODS
 from .costs import ConstantL2, GaussMLE, LinearMLE, LinearL2, KernelMSE
-from .costs import HarmonicL2
+from .costs import HarmonicL2, ConstantL1
