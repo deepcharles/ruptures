3453f99cbd182e8b99fd2bb6d3fff079c45d5dee
Charles <charles.truong@cmla.ens-cachan.fr>
2017-04-06 13:39:28 +0200

    Module utils Contient les fonctions utiles

---
+++ ruptures/utils/utils.py
@@ -0,0 +1,28 @@
+"""Miscellaneous functions for ruptures."""
+
+from itertools import tee
+
+from random import choice
+import numpy as np
+
+
+def pairwise(iterable):
+    "s -> (s0,s1), (s1,s2), (s2, s3), ..."
+    a, b = tee(iterable)
+    next(b, None)
+    return zip(a, b)
+
+
+def draw_bkps(n_samples, n_bkps):
+    """Return a set of random breakpoint indexes."""
+    bkps = [n_samples]
+    min_size = n_samples // (2 * n_bkps)
+    ind = np.arange(n_samples)
+    mask = np.ones(n_samples, dtype=bool)
+    mask[:min_size] = False
+    mask[-min_size:] = False
+    for _ in range(n_bkps):
+        bkp = choice(ind[mask])
+        mask[bkp - min_size:bkp + min_size] = False
+        bkps.append(bkp)
+    return sorted(bkps)
