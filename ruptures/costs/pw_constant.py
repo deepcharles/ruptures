077fbd8a7a56edb3c47a1c430e1b7010e5544816
Charles <charles.truong@cmla.ens-cachan.fr>
2017-04-06 18:37:52 +0200

    New cost functions L1 et L2 sont plus simples

--- ruptures/costs/pw_constant.py
+++ ruptures/costs/pw_constant.py
@@ -1,111 +1,46 @@
-from ruptures.search_methods import changepoint
-from ruptures.costs import NotEnoughPoints
+"""Cost functions for piecewise constant fonctions."""
 import numpy as np
+from ruptures.costs import NotEnoughPoints
 
 
-@changepoint
-class ConstantL2:
+def constantl2(signal):
+    """Return the L2 cost.
 
-    def error(self, start, end):
-        """
-        Cost: Squared error when approximating with a constant value (its
-        mean).
-        Associated K (see Pelt): 0
+    L2 norm of the residuals when approximating with a constant.
 
-        Args:
-            start (int): first index of the segment (index included)
-            end (int): last index of the segment (index excluded)
+    Args:
+        signal (array): signal (n_samples, n_features)
 
-        Returns:
-            float: L2 cost on the segment
+    Returns:
+        float: L2 cost
 
-        Raises:
-            NotEnoughPoints: if there are not enough points to compute the cost
-            for the specified segment (here, at least 2 points).
-        """
-
-        if end - start < 2:  # we need at least 2 points
-            raise NotEnoughPoints
-
-        sig = self.signal[start:end]
-        v = sig.var(axis=0).sum()
-        return v * (end - start)
-
-    def set_params(self):
-        pass
-
-    @property
-    def K(self):
-        return 0
+    Raises:
+        NotEnoughPoints: if there less than 2 samples
+    """
+    n_samples, _ = signal.shape
+    if n_samples < 2:
+        raise NotEnoughPoints
+    cost = signal.var(axis=0).sum() * n_samples
+    return cost
 
 
-@changepoint
-class GaussMLE:
+def constantl1(signal):
+    """Return the L1 cost.
 
-    def error(self, start, end):
-        """Associated K (see Pelt.__init__): 0
+    L1 norm of the residuals when approximating with a constant.
 
-        Args:
-            start (int): first index of the segment (index included)
-            end (int): last index of the segment (index excluded)
+    Args:
+        signal (array): signal (n_samples, n_features)
 
-        Raises:
-            NotEnoughPoints: if there are not enough points to compute the cost
-            for the specified segment (here, at least 2 points).
+    Returns:
+        float: L1 cost
 
-        Returns:
-            function: cost function. Here: - max log likelihood of a univariate
-            gaussian random variable.
-
-        """
-        assert 0 <= start <= end
-        if end - start < 2:  # we need at least 2 points
-            raise NotEnoughPoints
-
-        sig = self.signal[start:end]
-        v = sig.var(axis=0)
-        if np.sum(np.abs(v)) == 0:
-            return np.inf
-        res = 1 + np.log(2 * np.pi * v)
-        res = res.sum()
-        res *= (end - start) / 2
-        return res
-
-    def set_params(self):
-        pass
-
-    @property
-    def K(self):
-        return 0
-
-
-@changepoint
-class ConstantL1:
-
-    def error(self, start, end):
-        """Cost: minimum L1 error when approximating with a constant value.
-        Associated K (see Pelt): 0
-
-        Args:
-            start (int): first index of the segment (index included)
-            end (int): last index of the segment (index excluded)
-
-        Returns:
-            float: L1 cost on the segment
-
-        Raises:
-            NotEnoughPoints: if there are not enough points to compute the cost
-        """
-        if end - start < 2:  # we need at least 2 points
-            raise NotEnoughPoints
-
-        sig = self.signal[start:end]
-        v = np.abs(sig - np.median(sig, axis=0)).sum()
-        return v
-
-    def set_params(self):
-        pass
-
-    @property
-    def K(self):
-        return 0
+    Raises:
+        NotEnoughPoints: if there less than 2 samples
+    """
+    n_samples, _ = signal.shape
+    if n_samples < 2:
+        raise NotEnoughPoints
+    med = np.median(signal, axis=0)
+    cost = abs(signal - med).sum()
+    return cost
