0cd7387f2d2563f62698e99c0f98af9adac783a7
Charles <charles.truong@cmla.ens-cachan.fr>
2017-07-04 18:46:15 +0200

    methode à fenetre

--- ruptures/__init__.py
+++ ruptures/__init__.py
@@ -1,6 +1,6 @@
 """Import of modules"""
 from .costs import NotEnoughPoints
-from .search_methods import Dynp, Pelt, BottomUp, Binseg, Omp
+from .search_methods import Dynp, Pelt, BottomUp, Binseg, Omp, Window
 from .costs import LinearMLE, LinearL2, KernelMSE
 from .costs import HarmonicL2
 from .datasets import pw_constant
