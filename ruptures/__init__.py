ac93460862c68f020946737ef57199e3e2cca4eb
Charles <charles.truong@cmla.ens-cachan.fr>
2017-10-08 23:37:47 +0200

    import

--- ruptures/__init__.py
+++ ruptures/__init__.py
@@ -4,7 +4,7 @@ Offline change point detection for Python
 """
 
 from .exceptions import NotEnoughPoints
-from .datasets import pw_constant
+from .datasets import pw_constant, pw_normal, pw_linear, pw_wavy
 from .detection import (Binseg, BottomUp, Dynp, Omp, OmpK, Pelt, Window,
                         GreedyAR, GreedyLinear)
 from .show import display
