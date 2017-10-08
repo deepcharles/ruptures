81793ab541174af9db151286308c35741816bc82
Charles <charles.truong@cmla.ens-cachan.fr>
2017-10-08 23:35:28 +0200

    new draw_bkps

--- ruptures/utils/__init__.py
+++ ruptures/utils/__init__.py
@@ -1,3 +1,4 @@
 """Import utils functions."""
-from .utils import draw_bkps, pairwise, unzip, admissible_filter, sanity_check
+from .utils import pairwise, unzip, admissible_filter, sanity_check
 from .bnode import Bnode
+from .drawbkps import draw_bkps
---
+++ ruptures/utils/drawbkps.py
@@ -0,0 +1,12 @@
+r"""Draw a random partition."""
+
+import numpy as np
+from numpy.random import dirichlet
+
+
+def draw_bkps(n_samples=100, n_bkps=3):
+    """Draw a random partition with specified number of samples and specified number of changes."""
+    alpha = np.ones(n_bkps + 1) / (n_bkps + 1) * 2000
+    bkps = np.cumsum(dirichlet(alpha) * n_samples).astype(int).tolist()
+    bkps[-1] = n_samples
+    return bkps
