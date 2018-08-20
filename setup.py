ad222204f498a5144a575b55344f1740bd67434d
Charles <charles.truong@cmla.ens-cachan.fr>
2018-08-20 10:27:25 +0200

    Update setup.py new version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='ruptures',
-    version='1.0a1',
+    version='1.0.1',
     packages=find_packages(exclude=['docs', 'tests*', 'images']),
     install_requires=['numpy', 'scipy'],
     extras_require={
