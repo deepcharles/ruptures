85440181d2b9da687b5e526a1458efa38fd76605
Pete Y <pyork31@googlemail.com>
2019-03-06 10:59:56 +0000

    Begin work on gaussian mixture cost and dataset generation

---
+++ ruptures/costs/gaussian_mixture_cost.py
@@ -0,0 +1,31 @@
+from ruptures.base import BaseCost
+from sklearn.mixture import GaussianMixture
+
+
+class GaussianMixtureCost(BaseCost):
+    model = ""
+    min_size = 2
+
+    def __init__(self, n_components):
+        self.n_components = n_components
+        self.model = GaussianMixture(n_components=n_components)
+
+    def fit(self, signal):
+        """Set the internal parameter."""
+        self.signal = signal
+        return self
+
+    def error(self, start, end):
+        """Return the approximation cost on the segment [start:end].
+
+        Args:
+            start (int): start of the segment
+            end (int): end of the segment
+
+        Returns:
+            float: segment cost
+        """
+        sub = self.signal[start:end]
+
+        fitted_model = self.model.fit(sub)
+        return - fitted_model.score(sub)
---
+++ ruptures/datasets/pw_gaussian_mixture.py
@@ -0,0 +1,53 @@
+import numpy as np
+from numpy import random as rd
+
+from ruptures.utils import draw_bkps
+
+def select_mean_and_standard_deviation(ratio):
+    for i in range(len(ratio) - 1):
+        if rd.random() > ratio[i]:
+            return i
+    return len(ratio) - 1
+
+def get_gaussian_mixture_segment(n_features, ratio, means, standard_deviations, length):
+    result = np.empty((length, n_features), dtype=float)
+    for i in range(length):
+        mean_index = select_mean_and_standard_deviation(ratio)
+        result[i] = rd.normal(means[mean_index], standard_deviations[mean_index])
+    return result
+
+def generate_means_and_standard_deviations(n_components, noise_std, mean_range):
+    means = [0] * n_components
+    standard_deviations = [0] * n_components
+    mean_min, mean_max = mean_range
+    for i in range(n_components):
+        means[i] = rd.random() * (mean_max - mean_min)
+        if noise_std is not None:
+            standard_deviations[i] = rd.random() * noise_std
+    return means, standard_deviations
+
+def pw_gaussian_mixture(n_samples=200, n_features=1, n_bkps=3, noise_std=None,n_components=2, ratio=[0.8,0.2], mean_range=(1,10)):
+    """Return a piecewise constant signal and the associated changepoints.
+
+    Args:
+        n_samples (int): signal length
+        n_features (int, optional): number of dimensions
+        n_bkps (int, optional): number of changepoints
+        noise_std (float, optional): noise std. If None, no noise is added
+
+    Returns:
+        tuple: signal of shape (n_samples, n_features), list of breakpoints
+
+    """
+    breakpoints = draw_bkps(n_samples, n_bkps)
+    # we create the signal
+    signal = np.empty((n_samples, n_features), dtype=float)
+    indices = np.arange(n_samples)
+
+    for subarray in np.split(indices, breakpoints):
+        if subarray.size > 0:
+            means, standard_deviations = generate_means_and_standard_deviations(n_components, noise_std, mean_range)
+            gaussian_mixture_segment = get_gaussian_mixture_segment(n_features, ratio, means, standard_deviations, subarray.size)
+            signal[subarray] = gaussian_mixture_segment
+
+    return signal, breakpoints
