f087b1881cab8fbde6f1972fd286ba6b96ee1633
Charles Truong <charles@winglet.io>
2020-02-27 10:37:34 +0100

    version 1.0.3 published

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='ruptures',
-    version='1.0.1',
+    version='1.0.3',
     packages=find_packages(exclude=['docs', 'tests*', 'images']),
     install_requires=['numpy', 'scipy'],
     extras_require={
@@ -26,12 +26,12 @@ setup(
     ],
     long_description="""\
 Offline change point detection for Python.
--------------------------------------
+------------------------------------------
 
-__ruptures__ is a Python library for offline change point detection. This package provides methods for the analysis and segmentation of non-stationary signals. Implemented algorithms include exact and approximate detection for various parametric and non-parametric models. __ruptures__ focuses on ease of use by providing a well-documented and consistent interface. In addition, thanks to its modular structure, different algorithms and models can be connected and extended within this package.
+**ruptures** is a Python library for offline change point detection. This package provides methods for the analysis and segmentation of non-stationary signals. Implemented algorithms include exact and approximate detection for various parametric and non-parametric models. **ruptures** focuses on ease of use by providing a well-documented and consistent interface. In addition, thanks to its modular structure, different algorithms and models can be connected and extended within this package.
 
 
-An extensive documentation is available ctruong.perso.math.cnrs.fr/ruptures.
+An extensive documentation is available `ctruong.perso.math.cnrs.fr/ruptures <http://ctruong.perso.math.cnrs.fr/ruptures>`_.
 
 This version requires Python 3 or later.
 """
