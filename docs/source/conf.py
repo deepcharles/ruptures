77d4f3d0a1ee43569225508707b6f76e774b3d13
Charles T <charles@doffy.net>
2020-08-03 09:10:15 +0200

    Change theme to `press` in conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -92,7 +92,8 @@ todo_include_todos = True
 # a list of builtin themes.
 #
 # html_theme = 'sphinxdoc'
-html_theme = 'sphinx_rtd_theme'
+# html_theme = 'sphinx_rtd_theme'
+html_theme = "press"
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
