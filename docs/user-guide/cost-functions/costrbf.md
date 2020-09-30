5372ea25dac52362a15876fe6dde19945d2d7159
Charles Truong <charles@doffy.net>
2020-09-30 17:23:06 +0200

    add costrbf

---
+++ docs/user-guide/cost-functions/costrbf.md
@@ -0,0 +1,70 @@
+# Kernelized mean change (CostRbf)
+
+## Description
+
+Given a positive semi-definite kernel $k(\cdot, \cdot) : \mathbb{R}^d\times \mathbb{R}^d \mapsto \mathbb{R}$ and its associated feature map $\Phi:\mathbb{R}^d \mapsto \mathcal{H}$ (where $\mathcal{H}$ is an appropriate Hilbert space), this cost function detects changes in the mean of the embedded signal $\{\Phi(y_t)\}_t$ [[Garreau2018]](#Garreau2018) [[Arlot2019]](#Arlot2019).
+Formally, for a signal $\{y_t\}_t$ on an interval $I$,
+
+$$
+c(y_{I}) = \sum_{t\in I} \| \Phi(y_t) - \bar{\mu} \|_{\mathcal{H}}^2
+$$
+
+where $\bar{\mu}$ is the empirical mean of the embedded sub-signal $\{\Phi(y_t)\}_{t\in I}$.
+Here the kernel is the radial basis function (rbf):
+
+$$
+k(x, y) = \exp(-\gamma \| x - y \|^2 )
+$$
+
+where $\| \cdot \|$ is the Euclidean norm and $\gamma>0$ is the so-called bandwidth parameter and is determined according to median heuristics (i.e. equal to the inverse of median of all pairwise distances).
+
+In a nutshell, this cost function is able to detect changes in the distribution of an iid sequence of random variables.
+Because it is non-parametric, it is performs reasonably well on a wide range of tasks.
+
+## Usage
+
+Start with the usual imports and create a signal.
+
+```python
+import numpy as np
+import matplotlib.pylab as plt
+import ruptures as rpt
+# creation of data
+n, dim = 500, 3  # number of samples, dimension
+n_bkps, sigma = 3, 5  # number of change points, noise standart deviation
+signal, bkps = rpt.pw_constant(n, dim, n_bkps, noise_std=sigma)
+```
+
+Then create a [`CostRbf`][ruptures.costs.costrbf.CostRbf] instance and print the cost of the sub-signal `signal[50:150]`.
+
+```python
+c = rpt.costs.CostRbf().fit(signal)
+print(c.error(50, 150))
+```
+
+You can also compute the sum of costs for a given list of change points.
+
+```python
+print(c.sum_of_costs(bkps))
+print(c.sum_of_costs([10, 100, 200, 250, n]))
+```
+
+In order to use this cost class in a change point detection algorithm (inheriting from [`BaseEstimator`][ruptures.base.BaseEstimator]), either pass a [`CostRbf`][ruptures.costs.costrbf.CostRbf] instance (through the argument `custom_cost`) or set `model="rbf"`.
+
+```python
+c = rpt.costs.CostRbf(); algo = rpt.Dynp(custom_cost=c)
+# is equivalent to
+algo = rpt.Dynp(model="rbf")
+```
+
+::: ruptures.costs.costrbf.CostRbf
+    rendering:
+        show_root_heading: true
+
+## References
+
+<a id="Garreau2018">[Garreau2018]</a>
+Garreau, D., & Arlot, S. (2018). Consistent change-point detection with kernels. Electronic Journal of Statistics, 12(2), 4440–4486.
+
+<a id="Arlot2019">[Arlot2019]</a>
+Arlot, S., Celisse, A., & Harchaoui, Z. (2019). A kernel multiple change-point algorithm via model selection. Journal of Machine Learning Research, 20(162), 1–56.
--- ruptures/costs/costrbf.py
+++ ruptures/costs/costrbf.py
@@ -1,80 +1,4 @@
-r"""
-.. _sec-kernel:
-
-Kernelized mean change
-====================================================================================================
-
-Description
-----------------------------------------------------------------------------------------------------
-
-Given a positive semi-definite kernel :math:`k(\cdot, \cdot) : \mathbb{R}^d\times \mathbb{R}^d \mapsto \mathbb{R}` and its associated feature map :math:`\Phi:\mathbb{R}^d \mapsto \mathcal{H}` (where :math:`\mathcal{H}` is an appropriate Hilbert space), this cost function detects changes in the mean of the embedded signal :math:`\{\Phi(y_t)\}_t` :cite:`ker-arlot2012kernel,ker-gretton2012kernel`.
-Formally, for a signal :math:`\{y_t\}_t` on an interval :math:`I`,
-
-    .. math:: c(y_{I}) = \sum_{t\in I} \|\Phi(y_t) - \bar{\mu}\|_{\mathcal{H}}^2
-
-where :math:`\bar{\mu}` is the empirical mean of the embedded sub-signal :math:`\{\Phi(y_t)\}_{t\in I}`.
-Here the kernel is the radial basis function (rbf):
-
-    .. math:: k(x, y) = \exp(-\gamma \|x-y\|^2)
-
-where :math:`\|\cdot\|` is the Euclidean norm and :math:`\gamma>0` is the so-called bandwidth parameter and is determined according to median heuristics (i.e. equal to the inverse of median of all pairwise distances).
-
-Usage
-----------------------------------------------------------------------------------------------------
-
-Start with the usual imports and create a signal.
-
-.. code-block:: python
-
-    import numpy as np
-    import matplotlib.pylab as plt
-    import ruptures as rpt
-    # creation of data
-    n, dim = 500, 3  # number of samples, dimension
-    n_bkps, sigma = 3, 5  # number of change points, noise standart deviation
-    signal, bkps = rpt.pw_constant(n, dim, n_bkps, noise_std=sigma)
-
-Then create a :class:`CostRbf` instance and print the cost of the sub-signal :code:`signal[50:150]`.
-
-.. code-block:: python
-
-    c = rpt.costs.CostRbf().fit(signal)
-    print(c.error(50, 150))
-
-You can also compute the sum of costs for a given list of change points.
-
-.. code-block:: python
-
-    print(c.sum_of_costs(bkps))
-    print(c.sum_of_costs([10, 100, 200, 250, n]))
-
-
-In order to use this cost class in a change point detection algorithm (inheriting from :class:`BaseEstimator`), either pass a :class:`CostRbf` instance (through the argument ``'custom_cost'``) or set :code:`model="rbf"`.
-
-.. code-block:: python
-
-    c = rpt.costs.CostRbf(); algo = rpt.Dynp(custom_cost=c)
-    # is equivalent to
-    algo = rpt.Dynp(model="rbf")
-
-
-Code explanation
-----------------------------------------------------------------------------------------------------
-
-.. autoclass:: ruptures.costs.CostRbf
-    :members:
-    :special-members: __init__
-
-.. rubric:: References
-
-.. bibliography:: ../biblio.bib
-    :style: alpha
-    :cited:
-    :labelprefix: KER
-    :keyprefix: ker-
-
-
-"""
+r"""Kernelized mean change"""
 import numpy as np
 from scipy.spatial.distance import pdist, squareform
 
@@ -91,10 +15,11 @@ class CostRbf(BaseCost):
     model = "rbf"
 
     def __init__(self):
+        """Initialize the object."""
         self.gram = None
         self.min_size = 2
 
-    def fit(self, signal):
+    def fit(self, signal) -> "CostRbf":
         """Sets parameters of the instance.
 
         Args:
@@ -114,7 +39,7 @@ class CostRbf(BaseCost):
         self.gram = np.exp(squareform(-K))
         return self
 
-    def error(self, start, end):
+    def error(self, start, end) -> float:
         """Return the approximation cost on the segment [start:end].
 
         Args:
@@ -122,10 +47,10 @@ class CostRbf(BaseCost):
             end (int): end of the segment
 
         Returns:
-            float: segment cost
+            segment cost
 
         Raises:
-            NotEnoughPoints: when the segment is too short (less than ``'min_size'`` samples).
+            NotEnoughPoints: when the segment is too short (less than `min_size` samples).
         """
         if end - start < self.min_size:
             raise NotEnoughPoints
