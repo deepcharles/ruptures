2163fb9fbb674c1ad0fe18ee0f6119f8cfbe0393
Charles Truong <charles@doffy.net>
2020-10-22 16:45:48 +0200

    add hausdorff

---
+++ docs/code-reference/metrics/hausdorff.md
@@ -0,0 +1,5 @@
+# Hausdorff metric (`hausdorff`)
+
+::: ruptures.metrics.hausdorff.hausdorff
+    rendering:
+        show_root_heading: true
--- ruptures/metrics/hausdorff.py
+++ ruptures/metrics/hausdorff.py
@@ -1,43 +1,4 @@
-r"""
-.. _sec-hausdorff:
-
-Hausdorff metric
-====================================================================================================
-
-Description
-----------------------------------------------------------------------------------------------------
-
-The Hausdorff metric measures the worst prediction error.
-Assume a set of change point indexes :math:`t_1,t_2,\dots` and their estimates :math:`\hat{t}_1, \hat{t}_2,\dots`.
-The Hausdorff metric is then equal to
-
-    .. math:: \text{Hausdorff}(\{t_k\}_k, \{\hat{t}_k\}_k) :=  \max \{ \max_k \min_l |t_k - \hat{t}_l| \, , \max_k \min_l |\hat{t}_k - t_l|\}.
-
-
-.. figure:: /images/hausdorff.png
-   :scale: 50 %
-   :alt: hausdorff metric
-
-   Schematic example: true segmentation in gray, estimated segmentation in dashed lines. Here, Hausdorff is equal to :math:`\max(\Delta t_1, \Delta t_2, \Delta t_3)`.
-
-Usage
-----------------------------------------------------------------------------------------------------
-
-Start with the usual imports and create two segmentations to compare.
-
-.. code-block:: python
-
-    from ruptures.metrics import hausdorff
-    bkps1, bkps2 = [100, 200, 500], [105, 115, 350, 400, 500]
-    print(hausdorff(bkps1, bkps2))
-
-
-Code explanation
-----------------------------------------------------------------------------------------------------
-
-.. autofunction:: ruptures.metrics.hausdorff.hausdorff
-
-"""
+r"""Hausdorff metric"""
 import numpy as np
 from scipy.spatial.distance import cdist
 from ruptures.metrics.sanity_check import sanity_check
