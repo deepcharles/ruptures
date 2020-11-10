61248669f5bd7e5d387e15039c1399592dd331ad
Charles Truong <charles@doffy.net>
2020-11-10 15:06:41 +0100

    remove number of eligible changes from args - convert_path_matrix

--- ruptures/utils/_utils/convert_path_matrix_c.c
+++ ruptures/utils/_utils/convert_path_matrix_c.c
@@ -1,19 +1,19 @@
 #include <stdio.h>
+#include <math.h>
 
-void convert_path_matrix_c(int *path_matrix, int n_bkps, int n_eligible_bkps, int n_bkps_max, int jump, int *bkps_list)
+void convert_path_matrix_c(int *path_matrix, int n_bkps, int n_samples, int n_bkps_max, int jump, int *bkps_list)
 {
-    bkps_list[n_bkps] = n_eligible_bkps;
+    int q = (int)ceil((float)n_samples / (float)jump);
+    bkps_list[n_bkps] = q;
     int k = 0;
-    printf("Before while\n");
     while (k++ < n_bkps)
     {
-        printf("k is %d\n", k);
-        printf("%d\n", bkps_list[n_bkps - k + 1]);
         bkps_list[n_bkps - k] = path_matrix[bkps_list[n_bkps - k + 1] * (n_bkps_max + 1) + (n_bkps - k + 1)];
     }
     for (k = 0 ; k < n_bkps + 1 ; k++)
     {
         bkps_list[k] = bkps_list[k] * jump;
     }
+    bkps_list[n_bkps] = n_samples;
     return;
 }
