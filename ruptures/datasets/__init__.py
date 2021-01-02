af8bd7fd397c6bcdc18e5d079914449d8c231fba
Charles T <charles@doffy.net>
2021-01-02 21:08:54 +0100

    style: sort imports

--- ruptures/datasets/__init__.py
+++ ruptures/datasets/__init__.py
@@ -1,22 +1,5 @@
-"""
-====================================================================================================
-Synthetic signals
-====================================================================================================
-
-:mod:`ruptures.datasets` is designed to simplify synthetic signal generation.
-
-.. toctree::
-    :glob:
-    :maxdepth: 1
-
-    pw_constant
-    pw_normal
-    pw_linear
-    pw_wavy
-
-"""
-
+"""Utility functions to load or generate data sets."""
 from .pw_constant import pw_constant
-from .pw_normal import pw_normal
 from .pw_linear import pw_linear
+from .pw_normal import pw_normal
 from .pw_wavy import pw_wavy
