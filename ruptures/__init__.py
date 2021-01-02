cf77355cf0bf0b79ec2733611138438d9cc95676
Charles T <charles@doffy.net>
2021-01-02 18:48:29 +0100

    style: remove useless imports

--- ruptures/__init__.py
+++ ruptures/__init__.py
@@ -6,13 +6,4 @@ Offline change point detection for Python
 from .datasets import pw_constant, pw_linear, pw_normal, pw_wavy
 from .detection import Binseg, BottomUp, Dynp, KernelCPD, Pelt, Window
 from .exceptions import NotEnoughPoints
-from .datasets import pw_constant, pw_normal, pw_linear, pw_wavy
-from .detection import (
-    Binseg,
-    BottomUp,
-    Dynp,
-    KernelCPD,
-    Pelt,
-    Window,
-)
 from .show import display
