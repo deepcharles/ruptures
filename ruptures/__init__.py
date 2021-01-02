a018c4ccaf5adb7383e48a2b01d429ed032c9ed8
Charles T <charles@doffy.net>
2021-01-02 18:37:22 +0100

    style: sort imports

--- ruptures/__init__.py
+++ ruptures/__init__.py
@@ -3,6 +3,8 @@ Offline change point detection for Python
 ====================================================================================================
 """
 
+from .datasets import pw_constant, pw_linear, pw_normal, pw_wavy
+from .detection import Binseg, BottomUp, Dynp, KernelCPD, Pelt, Window
 from .exceptions import NotEnoughPoints
 from .datasets import pw_constant, pw_normal, pw_linear, pw_wavy
 from .detection import (
