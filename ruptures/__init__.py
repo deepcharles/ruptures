c4f89f4d885a4e4bc25a9120d0a289fe53eda474
Charles T <charles@doffy.net>
2021-01-02 18:55:16 +0100

    build: bump version (dev)

--- ruptures/__init__.py
+++ ruptures/__init__.py
@@ -4,3 +4,5 @@ from .datasets import pw_constant, pw_linear, pw_normal, pw_wavy
 from .detection import Binseg, BottomUp, Dynp, KernelCPD, Pelt, Window
 from .exceptions import NotEnoughPoints
 from .show import display
+
+__version__ = "1.1.2dev0"
