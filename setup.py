753df980627beac467ff82cf625b65fbc4eebd17
Charles T <charles@doffy.net>
2021-01-03 14:48:16 +0100

    build: change layout (use src folder) in setup.cfg

--- setup.py
+++ setup.py
@@ -4,17 +4,17 @@ ext_modules = [
     Extension(
         "ruptures.detection._detection.ekcpd",
         sources=[
-            "ruptures/detection/_detection/ekcpd.pyx",
-            "ruptures/detection/_detection/ekcpd_computation.c",
-            "ruptures/detection/_detection/ekcpd_pelt_computation.c",
-            "ruptures/detection/_detection/kernels.c",
+            "src/ruptures/detection/_detection/ekcpd.pyx",
+            "src/ruptures/detection/_detection/ekcpd_computation.c",
+            "src/ruptures/detection/_detection/ekcpd_pelt_computation.c",
+            "src/ruptures/detection/_detection/kernels.c",
         ],
     ),
     Extension(
         "ruptures.utils._utils.convert_path_matrix",
         sources=[
-            "ruptures/utils/_utils/convert_path_matrix.pyx",
-            "ruptures/utils/_utils/convert_path_matrix_c.c",
+            "src/ruptures/utils/_utils/convert_path_matrix.pyx",
+            "src/ruptures/utils/_utils/convert_path_matrix_c.c",
         ],
     ),
 ]
@@ -24,6 +24,5 @@ if __name__ == "__main__":
     from Cython.Build import cythonize
 
     setup(
-        packages=find_packages(exclude=["docs", "tests*", "images"]),
         ext_modules=cythonize(ext_modules, language_level="3"),
     )
