fix(CostNormal): add small bias in covariance matrix (#198)
* fix: add small bias on diagonal

to cope with signals with truly constant segments

* test: add test for the costnormal correction

* [pre-commit.ci] auto fixes from pre-commit.com hooks

for more information, see https://pre-commit.ci

* fix: more consistent behviour for constant signals

If the user asks n_bkps change-points, it will
return n_bkps change-points, even on constant
signals (before it returned none).

* fix: add warning about the changed behaviour

* style: add small comments+shorter comments

* style(exceptions): removing useless lines

* docs: documenting the changes

* test(costnormal): test both behaviours (old+new)

* test: new behaviour on constant signals

* [pre-commit.ci] auto fixes from pre-commit.com hooks

for more information, see https://pre-commit.ci

* style: remove import

* fix: removing weird if case

* style: sort imports

* test: adding test for bnode

* [pre-commit.ci] auto fixes from pre-commit.com hooks

for more information, see https://pre-commit.ci

* fix: warning only if the new behaviour is used

* test: add test for rpt.utils.bnode

* test: add test for window and costnormal

for the case `add_small_diag=False`

* style: fix typo in docstring

* style: simpler import

* test: update tests/test_bnode.py

Co-authored-by: Olivier Boulant <olivier.boulant@hey.com>

* Revert "fix: more consistent behviour for constant signals"

This reverts commit 3dca2f4750cf82821f47072dcef8c34fac43fd94.

* Revert "style: remove import"

This reverts commit 74ca7e96d319ea5986063b8397bd566924f4aa0f.

* fix: backward compatibility of Binseg+costnormal

If the user wants the old behaviour of costnormal
(ie `add_small_diag=False`), the result will be
the same as before

* test: add old behaviour or binseg+costnormal

* [pre-commit.ci] auto fixes from pre-commit.com hooks

for more information, see https://pre

src/ruptures/costs/costnormal.py
@@ -1,7 +1,8 @@
 r"""Gaussian process changes (CostNormal)"""
+import warnings
+
 import numpy as np
 from numpy.linalg import slogdet
-
 from ruptures.base import BaseCost
 from ruptures.costs import NotEnoughPoints
 
@@ -12,16 +13,31 @@ class CostNormal(BaseCost):
 
     model = "normal"
 
-    def __init__(self):
-        """Initialize the object."""
+    def __init__(self, add_small_diag=True):
+        """Initialize the object.
+
+        Args:
+            add_small_diag (bool, optional): For signals with truly constant
+                segments, the covariance matrix is badly conditioned, so we add
+                a small diagonal matrix. Defaults to True.
+        """
         self.signal = None
         self.min_size = 2
+        self.add_small_diag = add_small_diag
+        if add_small_diag:
+            warnings.warn(
+                "New behaviour in v1.1.5: "
+                "a small bias is added to the covariance matrix to cope with truly "
+                "constant segments (see PR#198).",
+                UserWarning,
+            )
 
     def fit(self, signal) -> "CostNormal":
         """Set parameters of the instance.
 
         Args:
-            signal (array): signal. Shape (n_samples,) or (n_samples, n_features)
+            signal (array): signal of shape (n_samples,) or
+                (n_samples, n_features)
 
         Returns:
             self
@@ -30,7 +46,7 @@ class CostNormal(BaseCost):
             self.signal = signal.reshape(-1, 1)
         else:
             self.signal = signal
-
+        self.n_samples, self.n_dims = self.signal.shape
         return self
 
     def error(self, start, end) -> float:
@@ -44,7 +60,8 @@ class CostNormal(BaseCost):
             segment cost
 
         Raises:
-            NotEnoughPoints: when the segment is too short (less than `min_size` samples).
+            NotEnoughPoints: when the segment is too short (less than `min_size`
+                samples).
         """
         if end - start < self.min_size:
             raise NotEnoughPoints
@@ -54,5 +71,7 @@ class CostNormal(BaseCost):
             cov = np.cov(sub.T)
         else:
             cov = np.array([[sub.var()]])
+        if self.add_small_diag:  # adding small bias
+            cov += 1e-6 * np.eye(self.n_dims)
         _, val = slogdet(cov)
         return val * (end - start)

src/ruptures/detection/binseg.py
@@ -1,10 +1,11 @@
 r"""Binary segmentation."""
 from functools import lru_cache
+
+import numpy as np
 from ruptures.base import BaseCost, BaseEstimator
 from ruptures.costs import cost_factory
-from ruptures.utils import pairwise, sanity_check
 from ruptures.exceptions import BadSegmentationParameters
-import numpy as np
+from ruptures.utils import pairwise, sanity_check
 
 
 class Binseg(BaseEstimator):
@@ -85,7 +86,7 @@ class Binseg(BaseEstimator):
     def single_bkp(self, start, end):
         """Return the optimal breakpoint of [start:end] (if it exists)."""
         segment_cost = self.cost.error(start, end)
-        if np.isinf(segment_cost) and segment_cost < 0:  # if constant on segment
+        if np.isinf(segment_cost) and segment_cost < 0:  # if cost is -inf
             return None, 0
         gain_list = list()
         for bkp in range(start, end, self.jump):

tests/test_costs.py
@@ -1,9 +1,8 @@
-import pytest
 import numpy as np
-from ruptures.costs import cost_factory, CostLinear
+import pytest
+from ruptures.costs import CostLinear, CostNormal, cost_factory
 from ruptures.datasets import pw_constant
 from ruptures.exceptions import NotEnoughPoints
-import numpy as np
 
 
 @pytest.fixture(scope="module")
@@ -120,7 +119,7 @@ def test_factory_exception():
 
 # Test CostLinear
 def test_costlinear(signal_bkps_5D_noisy, signal_bkps_1D_noisy):
-    # Creation of data. For convinience, we use
+    # Creation of data. For convenience, we use
     # already generated signal_bkps_5D and signal_bkps_1D
     signal_regressors, _ = signal_bkps_5D_noisy  # regressors
     signal, bkps = signal_bkps_1D_noisy  # observed signal
@@ -138,3 +137,29 @@ def test_costlinear(signal_bkps_5D_noisy, signal_bkps_1D_noisy):
     c.sum_of_costs(bkps)
     with pytest.raises(NotEnoughPoints):
         c.error(10, 11)
+
+
+# Test CostNormal
+def test_costnormal():
+    # For signals that have truly constant segments, CostNormal should not fail
+    # with the correction on the diagonal of the covariance matrix.
+
+    # generate data
+    signal_1D = np.r_[np.ones(100), np.arange(100), np.zeros(100)]
+    signal_2D = np.c_[signal_1D, signal_1D[::-1]]
+    n_samples = signal_1D.shape[0]
+    bkps = [100, 200, n_samples]
+
+    # test cost function
+    for signal in (signal_1D, signal_2D):
+        c = CostNormal(add_small_diag=True).fit(signal=signal)
+        c.error(0, 100)
+        c.error(100, n_samples)
+        c.error(10, 50)
+        c.sum_of_costs(bkps)
+        with pytest.raises(NotEnoughPoints):
+            c.error(10, 11)
+
+    # test cost function without correction
+    c = CostNormal(add_small_diag=False).fit(signal=signal_1D)
+    assert np.isinf(c.error(0, 100))