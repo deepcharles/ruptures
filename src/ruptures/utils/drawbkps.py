34b62c36cafa2b17723ad64e8ccbc0a40359cd82
Olivier Boulant <olivier.boulant@hey.com>
2021-12-06 12:04:18 +0100

    fix: propagate random seed

--- src/ruptures/utils/drawbkps.py
+++ src/ruptures/utils/drawbkps.py
@@ -1,13 +1,13 @@
 r"""Draw a random partition."""
 
 import numpy as np
-from numpy.random import dirichlet
 
 
-def draw_bkps(n_samples=100, n_bkps=3):
+def draw_bkps(n_samples=100, n_bkps=3, seed=None):
     """Draw a random partition with specified number of samples and specified
     number of changes."""
+    rng = np.random.default_rng(seed=seed)
     alpha = np.ones(n_bkps + 1) / (n_bkps + 1) * 2000
-    bkps = np.cumsum(dirichlet(alpha) * n_samples).astype(int).tolist()
+    bkps = np.cumsum(rng.dirichlet(alpha) * n_samples).astype(int).tolist()
     bkps[-1] = n_samples
     return bkps
