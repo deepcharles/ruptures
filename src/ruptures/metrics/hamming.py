a572c9ffb51a4e2f4c3c84db994b74fac6245337
Charles T <charles@doffy.net>
2021-12-15 16:16:02 +0100

    fix: hamming is derived from randindex

--- src/ruptures/metrics/hamming.py
+++ src/ruptures/metrics/hamming.py
@@ -1,19 +1,5 @@
 """Hamming metric for segmentation."""
-
-import numpy as np
-from scipy.sparse import triu
-
-from ruptures.metrics.sanity_check import sanity_check
-from ruptures.utils import pairwise
-
-
-def membership_mat(bkps):
-    """Return membership matrix for the given segmentation."""
-    n_samples = bkps[-1]
-    m_mat = np.zeros((n_samples, n_samples))
-    for start, end in pairwise([0] + bkps):
-        m_mat[start:end, start:end] = 1
-    return m_mat
+from ruptures.metrics.randindex import randindex
 
 
 def hamming(bkps1, bkps2):
@@ -28,10 +14,4 @@ def hamming(bkps1, bkps2):
     Returns:
         float: Hamming distance.
     """
-    sanity_check(bkps1, bkps2)
-    n_samples = max(bkps1)
-
-    disagreement = abs(membership_mat(bkps1) - membership_mat(bkps2))
-    disagreement = triu(disagreement, k=1).sum() * 1.0
-    disagreement /= n_samples * (n_samples - 1) / 2  # scaling
-    return disagreement
+    return 1-randindex(bkps1=bkps1, bkps2=bkps2)
