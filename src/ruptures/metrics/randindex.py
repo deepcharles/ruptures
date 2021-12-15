48bc2f7a4b59615fa9987686a5b11d040fae1de1
Charles T <charles@doffy.net>
2021-12-15 17:19:34 +0100

    perf: small changes

--- src/ruptures/metrics/randindex.py
+++ src/ruptures/metrics/randindex.py
@@ -1,7 +1,6 @@
 r"""Rand index (`randindex`)"""
 from ruptures.metrics.sanity_check import sanity_check
 from ruptures.utils import pairwise
-from itertools import islice
 
 
 def randindex(bkps1, bkps2):
@@ -17,20 +16,19 @@ def randindex(bkps1, bkps2):
     """
     sanity_check(bkps1, bkps2)
     n_samples = bkps1[-1]
-    disagreement = 0
+    bkps1_with_0 = [0] + bkps1
+    bkps2_with_0 = [0] + bkps2
 
+    disagreement = 0
     beginj = 0  # avoids unnecessary computations
-    for (start1, end1) in pairwise([0] + bkps1):
-        for (start2, end2) in islice(pairwise([0] + bkps2), beginj, None):
-            nij = min(end1, end2)
-            nij -= max(start1, start2)
-            nij = max(nij, 0)
+    for (start1, end1) in pairwise(bkps1_with_0):
+        for (start2, end2) in pairwise(bkps2_with_0[beginj:]):
+            nij = max(min(end1, end2)-max(start1, start2), 0)
             disagreement += nij * abs(end1 - end2)
 
             # we can skip the rest of the iteration, nij will be 0
             if end1 < end2:
                 break
-
             else:
                 beginj += 1
 
