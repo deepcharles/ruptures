324c802fdab50cb470b7d20f65a70f6251b8b819
Illviljan <14371165+Illviljan@users.noreply.github.com>
2022-01-28 22:27:17 +0100

    Remove unused code

--- asv_bench/benchmarks/__init__.py
+++ asv_bench/benchmarks/__init__.py
@@ -3,8 +3,6 @@ import os
 
 import numpy as np
 
-_counter = itertools.count()
-
 
 def parameterized(names, params):
     def decorator(func):
@@ -13,33 +11,6 @@ def parameterized(names, params):
         return func
 
     return decorator
-
-
-def randn(shape, frac_nan=None, chunks=None, seed=0):
-    rng = np.random.RandomState(seed)
-    if chunks is None:
-        x = rng.standard_normal(shape)
-    else:
-        import dask.array as da
-
-        rng = da.random.RandomState(seed)
-        x = rng.standard_normal(shape, chunks=chunks)
-
-    if frac_nan is not None:
-        inds = rng.choice(range(x.size), int(x.size * frac_nan))
-        x.flat[inds] = np.nan
-
-    return x
-
-
-def randint(low, high=None, size=None, frac_minus=None, seed=0):
-    rng = np.random.RandomState(seed)
-    x = rng.randint(low, high, size)
-    if frac_minus is not None:
-        inds = rng.choice(range(x.size), int(x.size * frac_minus))
-        x.flat[inds] = -1
-
-    return x
 
 
 def _skip_slow():
