6e3bb72059ff087caa0aa1ab1f150cc2104deda4
Pietro Agazzi <contact@agazzipietro.it>
2026-05-24 06:36:12 +0200

    chore!: drop Python 3.9 and update dev tooling (#371)

--- src/ruptures/metrics/hamming.py
+++ src/ruptures/metrics/hamming.py
@@ -4,8 +4,9 @@ from ruptures.metrics.randindex import randindex
 
 
 def hamming(bkps1, bkps2):
-    """Modified Hamming distance for partitions. For all pair of points (x, y),
-    x != y, the functions computes the number of times the two partitions
+    """Modified Hamming distance for partitions.
+
+    For all pair of points (x, y), x != y, the functions computes the number of times the two partitions
     disagree. The result is scaled to be within 0 and 1.
 
     Args:
